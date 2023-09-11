8aef62352801c74f9f9675ebcc2dbf2a76ce8536
scythe <kingoinabrian06@gmail.com>
2023-09-11 00:07:39 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the code block prints out the characters in the parantheses
+ *
+ * Return: the return value should be zero
+ */
+int main(void)
+{
+	putc("\ "Programming is like building of a multiligual puzzle");
+	return (0);
+}
