9bcd4014efb94d015a67b74ef7cdbfc7b3443f40
scythe <kingoinabrian06@gmail.com>
2023-09-11 19:21:41 -0700

    sleep

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: value zero
+ */
+int main(void)
+{
+	char i;
+
+	 for (i = 'a' ; i <= 'z' ; i++)
+		 putchar(i);
+	 putchar("\n");
+	 return (0);
+}
