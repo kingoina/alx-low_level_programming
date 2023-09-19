c20c1d86ce9d8e17c2ee7c169d806368fdb2ccd7
scythe <kingoinabrian06@gmail.com>
2023-09-19 13:02:54 -0700

    random

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi -this function converts a string to an integer
+ * @s: is string to be converted
  *
- * Return: the int converted from the string
+ * Return: the interger converted from the string
  */
 int _atoi(char *s)
 {
