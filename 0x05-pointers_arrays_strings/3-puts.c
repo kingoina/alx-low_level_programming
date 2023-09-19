37a086bdffac92da44c51aa763f71c9e2b3e3572
scythe <kingoinabrian06@gmail.com>
2023-09-19 12:29:16 -0700

    reverse string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
+ * _puts - prints a string to the standard output
  * @str: pointer to the string to print
  */
 void _puts(char *str)
